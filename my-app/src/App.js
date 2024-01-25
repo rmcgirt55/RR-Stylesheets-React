7b8f58bf37038e696dd360fbb302ae23ddda160f
Richard McGirt <richardmcgirt@Richards-Mac-mini.local>
2024-01-24 20:44:30 -0500

    finish

---
+++ my-app/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import TextStyling from './Components/textStyling';
+import './Components/StylesheetOne.css'
+
+const stylesheet = {
+  fontSize: "25px",
+  textAlign: "center",
+  margin: "40px",
+  border: "1px solid green",
+  color: "red"
+};
+
+export default function App() {
+  return (
+    <div className="App">
+      <TextStyling stylesheet={stylesheet} />
+      <div className="StyledText">
+      <h1>Boo</h1>
+    </div>
+    </div>
+
+  );
+}
